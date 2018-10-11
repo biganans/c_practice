aae0346bfc1cc44bbe143da414f34662a6f371fe
大菠萝 <2062152083@qq.com>
2018-10-11 16:37:07 +0800

    Create bubble_sort.c

---
+++ code/bubble_sort.c
@@ -0,0 +1,50 @@
+//冒泡算法
+
+#include <stdio.h>
+
+void print_list(int* list,int lenght)
+{
+    for (int i=0;i<lenght;i++)
+    {
+        printf("%d ",list[i]);
+    }
+    printf("\n");
+}
+
+void bubble_sort(int* list,int lenght)
+{
+    print_list(list,lenght);
+    int i=0,j=0;
+    int temp = 0;
+    int flag = 0;
+    for (i=0;i<lenght;i++)
+    {
+        flag = 1;
+        //把最大的数冒泡到最后面 i代表已经冒泡了的个数
+        for (j=0;j<lenght-i-1;j++)
+        {
+            if (list[j+1] < list[j])
+            {
+                flag = 0;
+                //change
+                temp = list[j+1];
+                list[j+1] = list[j];
+                list[j] = temp;
+            }
+        }
+        //说明当前已经是排序完毕
+        if (1 == flag)
+        {
+            break;
+        }
+        print_list(list,lenght);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int list[] = {23,38,22,45,23,67,31,15,41};
+    int lenght = 10;
+    bubble_sort(list,lenght);
+    return 0;
+}
