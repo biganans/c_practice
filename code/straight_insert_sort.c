8c3961b1b9ed3436e1da63f01c8f4d272db2c7c8
大菠萝 <2062152083@qq.com>
2018-10-12 13:47:07 +0800

    Update straight_insert_sort.c

--- code/straight_insert_sort.c
+++ code/straight_insert_sort.c
@@ -2,22 +2,22 @@
 //O(n^2)
 #include <stdio.h>
 
-void print_list(int* list,int lenght)
+void print_list(int* list,int length)
 {
-    for (int i=0;i<lenght;i++)
+    for (int i=0;i<length;i++)
     {
         printf("%d ",list[i]);
     }
     printf("\n");
 }
 
-void straight_insert_sort(int* list,int lenght)
+void straight_insert_sort(int* list,int length)
 {
-    print_list(list,lenght);
+    print_list(list,length);
     int i=0,j=0;
     int k = 0;
     int tmp = 0;
-    for (i=1;i<lenght;i++)
+    for (i=1;i<length;i++)
     {
         tmp = list[i];
         j=i-1;
@@ -28,14 +28,14 @@ void straight_insert_sort(int* list,int lenght)
             j--;
         }
         list[j+1] = tmp;
-        print_list(list,lenght);
+        print_list(list,length);
     }
 }
 
 int main(int argc, char const *argv[])
 {
     int a[] = {7,4,-2,19,13,42,6,20};
-    int lenght = 8;
-    straight_insert_sort(a,lenght);
+    int length = 8;
+    straight_insert_sort(a,length);
     return 0;
 }
