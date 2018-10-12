0b6a69aed2067522dd67d46656c2388aad359a10
大菠萝 <2062152083@qq.com>
2018-10-12 13:44:39 +0800

    Update bubble_sort.c

--- code/bubble_sort.c
+++ code/bubble_sort.c
@@ -2,26 +2,26 @@
 //O(n^2)
 #include <stdio.h>
 
-void print_list(int* list,int lenght)
+void print_list(int* list,int length)
 {
-    for (int i=0;i<lenght;i++)
+    for (int i=0;i<length;i++)
     {
         printf("%d ",list[i]);
     }
     printf("\n");
 }
 
-void bubble_sort(int* list,int lenght)
+void bubble_sort(int* list,int length)
 {
-    print_list(list,lenght);
+    print_list(list,length);
     int i=0,j=0;
     int temp = 0;
     int flag = 0;
-    for (i=0;i<lenght;i++)
+    for (i=0;i<length;i++)
     {
         flag = 1;
         //把最大的数冒泡到最后面 i代表已经冒泡了的个数
-        for (j=0;j<lenght-i-1;j++)
+        for (j=0;j<length-i-1;j++)
         {
             if (list[j+1] < list[j])
             {
@@ -37,14 +37,14 @@ void bubble_sort(int* list,int lenght)
         {
             break;
         }
-        print_list(list,lenght);
+        print_list(list,length);
     }
 }
 
 int main(int argc, char const *argv[])
 {
     int list[] = {23,38,22,45,23,67,31,15,41};
-    int lenght = 10;
-    bubble_sort(list,lenght);
+    int length = 10;
+    bubble_sort(list,length);
     return 0;
 }
