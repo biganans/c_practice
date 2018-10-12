ba5281f04fb42f7b3534133471c7d0cc39ae3e4d
大菠萝 <2062152083@qq.com>
2018-10-12 13:46:42 +0800

    Update simple_selection_sort.c

--- code/simple_selection_sort.c
+++ code/simple_selection_sort.c
@@ -2,25 +2,25 @@
 //T(n) = O(n^2) S(n)=O(1)
 #include <stdio.h>
 
-void print_list(int* list,int lenght)
+void print_list(int* list,int length)
 {
-    for (int i=0;i<lenght;i++)
+    for (int i=0;i<length;i++)
     {
         printf("%d ",list[i]);
     }
     printf("\n");
 }
 
-void simple_selection_sort(int* list,int lenght)
+void simple_selection_sort(int* list,int length)
 {
     //后面的比前面选定的数小则交换位置
     int j = 0;
     int k = 0;
     int temp = 0;
-    for (int i=0;i<lenght;i++)
+    for (int i=0;i<length;i++)
     {
         k = i;
-        for (j=i+1;j<lenght;j++)
+        for (j=i+1;j<length;j++)
         {
             if (list[j] < list[i])
             {
@@ -30,15 +30,15 @@ void simple_selection_sort(int* list,int lenght)
             }
         }
 
-        print_list(list,lenght);
+        print_list(list,length);
     }
 }
 
 int main(int argc, char const *argv[])
 {
     int list[] = {7,4,-2,19,13,6};
-    int lenght = 6;
-    print_list(list,lenght);
+    int length = 6;
+    print_list(list,length);
     simple_selection_sort(list,lenght);
     return 0;
 }
