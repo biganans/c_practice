91dd054c2932376e713aab1d0c598d435e8868ff
大菠萝 <2062152083@qq.com>
2018-10-12 13:46:05 +0800

    Update shell_sort.c

--- code/shell_sort.c
+++ code/shell_sort.c
@@ -2,9 +2,9 @@
 //O(n^2)
 #include <stdio.h>
 
-void print_list(int* list,int lenght)
+void print_list(int* list,int length)
 {
-    for (int i=0;i<lenght;i++)
+    for (int i=0;i<length;i++)
     {
         printf("%d ",list[i]);
     }
@@ -12,12 +12,12 @@ void print_list(int* list,int lenght)
 }
 
 //d为排序的间距，依次变小
-void shell_pass(int* list,int lenght,int d)
+void shell_pass(int* list,int length,int d)
 {
     int k = 0;
     int temp = 0;
     //需要排序的次数
-    for (int i=d;i<lenght;i++)
+    for (int i=d;i<length;i++)
     {
         temp = list[i];
         k = i-d;
@@ -29,15 +29,15 @@ void shell_pass(int* list,int lenght,int d)
     }
 }
 
-void shell_sort(int* list,int lenght)
+void shell_sort(int* list,int length)
 {
-    print_list(list,lenght);
+    print_list(list,length);
     int d = lenght/2;
     while(d >=1)
     {
-        shell_pass(list,lenght,d);
+        shell_pass(list,length,d);
         d = d/2;
-        print_list(list,lenght);
+        print_list(list,length);
     }
 
 }
@@ -45,7 +45,7 @@ void shell_sort(int* list,int lenght)
 int main(int argc, char const *argv[])
 {
     int a[] = {9,13,8,2,5,13,7,1,15,11};
-    int lenght = 10;
-    shell_sort(a,lenght);
+    int length = 10;
+    shell_sort(a,length);
     return 0;
 }
